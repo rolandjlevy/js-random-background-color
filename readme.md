07e72799712a7af3f18c02497a7d3de1c7e7b0fa
Roland Levy <rolandjlevy@gmail.com>
2020-08-05 16:42:55 +0000

    Refactored

--- readme.md
+++ readme.md
@@ -1,8 +1,8 @@
-# Vanilla Javascript: random background colour generator
+# Random background colour generator with Vanilla JavaScript
 
 Generates a random background and displays the hex colour value
 
 #### Links
-- Live demo: https://js-random-background-colour.rjlevy.repl.co/
-- Source code: https://repl.it/@rjlevy/js-random-background-colour
-- Github: https://github.com/rolandjlevy/js-random-background-colour
+- [Live demo](https://js-random-background-colour.rjlevy.repl.co/)
+- [Source code](https://repl.it/@rjlevy/js-random-background-colour)
+- [Github] (https://github.com/rolandjlevy/js-random-background-colour)
--- script.js
+++ script.js
@@ -1,38 +1,45 @@
-const header = document.querySelector('.header');
-const btn = document.querySelector('.btn');
-let timer = false;
+document.addEventListener('DOMContentLoaded', () => {
 
-const base = 16;
-const hexStringLength = 6;
-const hex = [];
-let counter = 0;
+  const header = document.querySelector('.header');
+  const btn = document.querySelector('.btn');
 
-while (counter < base) {
-  let hexChar = counter.toString(base);
-  hex.push(hexChar);
-  counter++;
-}
+  const base = 16;
+  const hexStringLength = 6;
+  const hexChars = [];
+  let counter = 0;
+  let timer = false;
 
-btn.addEventListener('mousedown', (e) => {
-  timer = setInterval(() => {
+  while (++counter < base) {
+    let char = counter.toString(base);
+    hexChars.push(char);
+  }
+
+  btn.addEventListener('mousedown', (e) => {
+    timer = setInterval(randomise, 1);
+  });
+
+  btn.addEventListener('mouseup', (e) => {
+    clearInterval(timer);
+  });
+
+  const generateHexString = () => {
+    let result = '#';
+    let counter = 0;
+    while (++counter <= hexStringLength) {
+      const n = getRandomNum(hexChars.length-1);
+      result += hexChars[n];
+    }
+    return result;
+  }
+
+  const getRandomNum = (n) => Math.round(Math.random() * n);
+
+  function randomise() {
     const colour = generateHexString();
     document.body.style.backgroundColor = colour;
     header.innerHTML = colour;
-  }, 1);
-});
-
-btn.addEventListener('mouseup', (e) => {
-  clearInterval(timer);
-});
-
-const generateHexString = () => {
-  let result = '#';
-  let counter = 0;
-  while (++counter <= hexStringLength) {
-    const n = getRandomNum(hex.length-1);
-    result += hex[n];
   }
-  return result;
-}
 
-const getRandomNum = (n) => Math.round(Math.random() * n);
+  randomise();
+
+});
