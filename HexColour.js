0a03b26f1b2f06f21cd212083c8ce0f54ae8c798
Roland Levy <rolandjlevy@gmail.com>
2020-09-02 19:33:48 +0000

    Refactored to oop version

---
+++ HexColour.js
@@ -0,0 +1,43 @@
+class HexColour {
+  constructor(header) {
+    this.header = header;
+    this.init();
+    this.generateHexChars();
+    this.randomise();
+    this.detransition();
+  }
+  init() {
+    this.hexStringLength = 6;
+    this.hexChars = [];
+    this.counter = 0;
+    this.timer = null;
+  }
+  generateHexChars() {
+    const base = 16;
+    while (++this.counter < base) {
+      let char = this.counter.toString(base);
+      this.hexChars.push(char);
+    }
+  }
+  generateHexString() {
+    let result = '#';
+    this.counter = 0;
+    while (++this.counter <= this.hexStringLength) {
+      const n = Math.round(Math.random() * (this.hexChars.length-1));
+      result += this.hexChars[n];
+    }
+    return result;
+  }
+  randomise() {
+    const colour = this.generateHexString();
+    document.body.style.backgroundColor = colour;
+    this.header.innerHTML = colour;
+  }
+  detransition() {
+    setTimeout(() => {
+      document.body.classList.add('detransition');
+    }, 500);
+  }
+}
+
+
--- script.js
+++ script.js
@@ -1,45 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  let timer;
   const header = document.querySelector('.header');
   const btn = document.querySelector('.btn');
-
-  const base = 16;
-  const hexStringLength = 6;
-  const hexChars = [];
-  let counter = 0;
-  let timer = false;
-
-  while (++counter < base) {
-    let char = counter.toString(base);
-    hexChars.push(char);
-  }
+  const hexColour = new HexColour(header);
 
   btn.addEventListener('mousedown', (e) => {
-    timer = setInterval(randomise, 1);
+    timer = setInterval(() => {
+      hexColour.randomise();
+    }, 1);
   });
 
   btn.addEventListener('mouseup', (e) => {
     clearInterval(timer);
   });
-
-  const generateHexString = () => {
-    let result = '#';
-    let counter = 0;
-    while (++counter <= hexStringLength) {
-      const n = getRandomNum(hexChars.length-1);
-      result += hexChars[n];
-    }
-    return result;
-  }
-
-  const getRandomNum = (n) => Math.round(Math.random() * n);
-
-  function randomise() {
-    const colour = generateHexString();
-    document.body.style.backgroundColor = colour;
-    header.innerHTML = colour;
-  }
-
-  randomise();
 
 });
